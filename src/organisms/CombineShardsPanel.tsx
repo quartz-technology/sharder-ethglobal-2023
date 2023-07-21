20fae571f1c93ab491e1d873debfc60be2c4137b
cleopha <coline@seguret.net>
2023-07-21 21:37:06 +0200

    feat(ui): add skeleton for combine shards panel
    Signed-off-by: cleopha <coline@seguret.net>

--- src/organisms/CombineShardsPanel.tsx
+++ src/organisms/CombineShardsPanel.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import {
+    Button,
+    Fade,
+    Paper
+} from "@mui/material";
 
 export default function CombineShardsPanel(): JSX.Element {
     return (
-        <div style={{ display: "flex", flex: 1 }}>
-
-        </div>
+        <Fade in={true} style={{ transitionDelay: "200ms" }}>
+            <div style={{ display: "flex", flex: 1, flexDirection: "column", marginTop: "12px" }}>
+                <Paper sx={{ display: "flex", flexDirection: "column", height: "100%" }}>
+                    <div style={{ display: "flex", flex: 1 }} >
+                        {/*  TODO add Drag n Drop box  */}
+                    </div>
+                    <div style={{ display: "flex", justifyContent: "flex-end", margin: "12px"}}>
+                        <Button>Submit</Button>
+                    </div>
+                </Paper>
+            </div>
+        </Fade>
     );
 }
